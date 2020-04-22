da0c38218b7963533efcf8cc699fb7e943265f13
shuangyangcoco <675312794@qq.com>
2020-04-22 02:16:00 -0700

    style(cli.py):add cli file description

---
+++ swagger2case/cli.py
@@ -0,0 +1,10 @@
+""" Convert swagger api file to YAML/JSON testcase for HttpRunner.
+Usage:
+    # convert to JSON format testcase
+    >>> swagger2case demo.json
+    # convert to YAML format testcase
+    >>> hswagger2case demo.json -2y
+"""
+
+def main():
+    pass
