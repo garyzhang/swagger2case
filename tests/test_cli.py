5082533229ac6c3d8c67fb3754c714f3513094ef
shuangyangcoco <675312794@qq.com>
2020-04-24 02:43:12 -0700

    feat(test_cli.py):add test code for cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,19 +2,20 @@ import pytest
 import sys
 import io
 from swagger2case.cli import main
+from swagger2case.__about__ import __version__
+from pytest import raises
 
 
-class Test_Cli(object):
-
-    @classmethod
-    def setup_class(cls):
-        cls.capture_ouput = io.StringIO()
-        sys.stdout = cls.capture_ouput
+class TestCli(object):
 
     def test_show_version(self):
+        capture_ouput = io.StringIO()
+        sys.stdout = capture_ouput
         sys.argv = ["swagger2case", "-V"]
+        with raises(SystemExit):
+            main()
+        assert __version__ == capture_ouput.getvalue().strip()
 
-        from swagger2case.__about__ import __version__
-        assert __version__ == self.capture_ouput
+    
 
 
